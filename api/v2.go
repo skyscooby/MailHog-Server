46573f95c8e6feb644e768efbb94cc1fe6a1dc09
skyscooby <4450935+skyscooby@users.noreply.github.com>
2024-12-03 12:53:27 -0600

    Update v2.go

--- api/v2.go
+++ api/v2.go
@@ -7,9 +7,9 @@ import (
 
 	"github.com/gorilla/pat"
 	"github.com/ian-kent/go-log/log"
-	"github.com/mailhog/MailHog-Server/config"
-	"github.com/mailhog/MailHog-Server/monkey"
-	"github.com/mailhog/MailHog-Server/websockets"
+	"github.com/skyscooby/MailHog-Server/config"
+	"github.com/skyscooby/MailHog-Server/monkey"
+	"github.com/skyscooby/MailHog-Server/websockets"
 	"github.com/mailhog/data"
 )
 
