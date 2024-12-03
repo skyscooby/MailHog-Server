ce2ec5cd308763891a0a274445050fe7054beb83
skyscooby <4450935+skyscooby@users.noreply.github.com>
2024-12-03 12:54:01 -0600

    Update config.go

--- config/config.go
+++ config/config.go
@@ -7,7 +7,7 @@ import (
 	"log"
 
 	"github.com/ian-kent/envconf"
-	"github.com/mailhog/MailHog-Server/monkey"
+	"github.com/skyscooby/MailHog-Server/monkey"
 	"github.com/mailhog/data"
 	"github.com/mailhog/storage"
 )
